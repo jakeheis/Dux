e1248813afe9d76eeb1a5d78655ceae6f518026f
Jake Heiser <jakeheiser1@gmail.com>
2021-09-15 09:22:52 -0600

    Initial Commit

---
+++ SherpaTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SherpaTest
+//
+//  Created by Jake Heiser on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SherpaTest/SherpaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherpaTestApp.swift
+//  SherpaTest
+//
+//  Created by Jake Heiser on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SherpaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
