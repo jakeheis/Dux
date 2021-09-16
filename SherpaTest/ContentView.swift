1950904c338afaa01c78613f16f425a5cda96180
Jake Heiser <jakeheiser1@gmail.com>
2021-09-16 15:59:37 -0600

    Prototype

--- SherpaTest/ContentView.swift
+++ SherpaTest/ContentView.swift
@@ -9,9 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SherpaView {
+            TabView {
+                NavigationView {
+                    HomeView()
+                }.tabItem { Label("Home", systemImage: "house") }
+                Text("Hey").tabItem { Label("Profile", systemImage: "person.crop.circle") }
+            }
+        }
     }
+}
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            NavigationLink(destination: DetailView()) {
+                Text("Detail view")
+            }
+        }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView: View {
+    @EnvironmentObject var sherpa: SherpaGuide
+    
+    var body: some View {
+        VStack {
+            Text("Details")
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                sherpa.show = true
+            }
+        }
+    }
+}
+
+struct SherpaView<Content: View>: View {
+    @StateObject var guide = SherpaGuide()
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        content
+            .environmentObject(guide)
+            .overlay(overlay)
+    }
+    
+    @ViewBuilder
+    var overlay: some View {
+        if guide.show {
+            Color.black
+                .opacity(0.3)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture { guide.show.toggle() }
+        } else {
+            EmptyView()
+        }
+    }
+}
+
+class SherpaGuide: ObservableObject {
+    @Published var show = false
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SherpaTest/Sherpa.swift
@@ -0,0 +1,9 @@
+//
+//  Sherpa.swift
+//  SherpaTest
+//
+//  Created by Jake Heiser on 9/15/21.
+//
+
+import SwiftUI
+
